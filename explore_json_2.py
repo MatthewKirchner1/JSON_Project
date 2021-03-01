2a82fdd84ca80e9d547cec5fe618bcd4a7196989
Matthew Kirchner <Matthew_Kirchner1@baylor.edu>
2021-03-01 10:14:37 -0600

    finished py1 and py2

---
+++ explore_json_2.py
@@ -0,0 +1,58 @@
+import json
+import plotly
+
+infile = open("eq_data_1_day_m1.json", "r")
+
+outfile = open("readable_eq_data.json", "w")
+
+# Converts the data to something pthon can read
+
+eq_data = json.load(infile)
+
+json.dump(eq_data, outfile, indent=4)
+
+"""
+print(eq_data["features"][0]["properties"]["mag"])
+"""
+
+list_of_eqs = eq_data["features"]
+
+mags, lons, lats = [], [], []
+
+for eq in list_of_eqs:
+    mag = eq["properties"]["mag"]
+    lon = eq["geometry"]["coordinates"][0]
+    lat = eq["geometry"]["coordinates"][1]
+
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(lons[:10])
+print(lats[:10])
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "marker": {"size": [5 * mag for mag in mags]},
+        "colorscale": "Viridis",
+        "reversescale": True,
+        "colorbar": {"title": "Magnitude"},
+    }
+]
+
+data = [Scattergeo(lon=lons, lat=lats)]
+
+mylayout = {}
+
+mylayout["title"] = "Global Earthquakes"
+
+fig = {"data": data, "layout": mylayout}
+
+offline.plot(fig, filename="global_earthquakes.html")
